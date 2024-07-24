6145124b60107628145058bcc65d46621109abb5
HalilhanSAYIN <dev.halilhansayin@gmail.com>
2024-07-24 17:52:47 +0300

    catalog added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container :{
+      center: true,
+screens:{
+  lg:'1140px',
+  xl:'1140px',
+  '2xl':'1140px',
+}
+    },
+    extend: {
+      fontFamily : {
+        gemunu : ['Gemunu Libre', 'sans-serif'],
+        open : ['Open Sans','sans-serif']
+      },
+
+      colors:{
+        'gega-red' : '#BC1A45',
+        'gega-melon' : '#FFD369',
+        'gega-grey' : '#DDDDDD',
+        'gega-white' : '#F7F7F7',
+      },
+      spacing:{
+        '128': '32rem'
+      }
+    },
   },
   plugins: [],
 }
